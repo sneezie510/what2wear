ea41bfd4e192ef5760e7eee1511998c3bfd9af1d
Ruby Kim <rubyk70231@gmail.com>
2017-09-27 23:31:20 +0200

    added ffaker gem and basic user seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'ffaker'
+
+puts 'Creating 100 fake users...'
+100.times do
+  user = User.new(
+    name:    "#{FFaker::NameNL.first_name_female}, #{Faker::NameNL.last_name}",
+    address: "#{FFaker::AddressNL.street_address}, #{FFaker::AddressNL.city}",
+    email:  "#{FFaker::Internet.disposable_email}"
+  )
+  user.save!
+end
+puts 'Finished!'
